b9bf05a59d83abe97bb7863a880e068755000ac8
JD Arthur <jdarthur@gatech.edu>
2020-04-22 19:07:04 -0400

    added some styling + props for Question

--- client/src/Question.jsx
+++ client/src/Question.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './Question.css';
 
-function Question() {
-  return (
-    <div className="question">
-        question
-    </div>
-  );
+class Question extends React.Component {
+  render() {
+    return (
+      <div className="question_container"> 
+  		<div className="category">
+            {this.props.category}
+        </div>
+  		<div className="question">
+  			{this.props.question}
+        </div>
+        <div className="answer">
+        	{this.props.answer}
+        </div>
+  	</div>
+    );
+  }
 }
 
 export default Question;
--- client/src/Round.jsx
+++ client/src/Round.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Round.css';
+import Question from "./Question.jsx";
 
-function Round() {
-  return (
-    <div className="round">
-        round
-    </div>
-  );
+class Round extends React.Component {
+  render() {
+    return (
+      <div className="round">
+        <Question category="Geography" answer="Montpelier"
+                  question="What is the capital of Vermont?"/>
+      </div>
+    );
+  }
 }
 
 export default Round;
