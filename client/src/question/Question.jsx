04c41b28159f4bc5bb0da482c65b94964ca26b99
JD Arthur <jdarthur@gatech.edu>
2020-04-25 14:25:42 -0400

    client-side question editing

--- client/src/question/Question.jsx
+++ client/src/question/Question.jsx
@@ -1,36 +1,53 @@
 import React from 'react';
 import './Question.css';
 
+//JSON keys
+const CATEGORY = "category"
+const QUESTION = "question"
+const ANSWER = "answer"
+
 class Question extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      selected: false
-    }
-  }
 
   set_selected = (event) => {
-    this.setState({selected: true})
+    this.props.set_selected(this.props.id)
   }
 
+  set_category = (event) => {
+    this.props.set(this.props.id, CATEGORY, event.target.value)
+  }
+
+  set_question = (event) => {
+    this.props.set(this.props.id, QUESTION, event.target.value)
+  }
+
+  set_answer = (event) => {
+    this.props.set(this.props.id, ANSWER, event.target.value)
+  }
 
   render() {
-    const containerClass = "question_container" + (this.state.selected ? " selected" : "")
-    console.log(containerClass)
-    return (
-      <div className={containerClass}
-           onClick={this.set_selected}> 
-  		<div className="category">
-            {this.props.category}
+    if (this.props.selected) {
+      const containerClass = "question_container selected"
+      return (
+        <div className={containerClass} onClick={this.set_selected}>
+          <input className="category" value={this.props.category}
+            onChange={this.set_category} />
+          <input className="question" value={this.props.question}
+            onChange={this.set_question}/>
+          <input className="answer"   value={this.props.answer}
+            onChange={this.set_answer}/>
         </div>
-  		<div className="question">
-  			{this.props.question}
+      );
+    }
+    else {
+      const containerClass = "question_container"
+      return (
+        <div className={containerClass} onClick={this.set_selected}>
+          <div className="category"> {this.props.category} </div>
+          <div className="question"> {this.props.question} </div>
+          <div className="answer">   {this.props.answer}   </div>
         </div>
-        <div className="answer">
-        	{this.props.answer}
-        </div>
-  	</div>
-    );
+      );
+    }
   }
 }
 
--- client/src/question/QuestionList.jsx
+++ client/src/question/QuestionList.jsx
@@ -4,22 +4,23 @@ import './QuestionList.css';
 import Question from "./Question.jsx"
 
 
-const questions = [
-  {
-    question: "q",
-    answer: "a",
-    category: "c",
-    id: 1234
-  }
-]
+// const questions = [
+//   {
+//     question: "q",
+//     answer: "a",
+//     category: "c",
+//     id: 1234
+//   }
+// ]
 
 class QuestionList extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      questions: questions,
+      questions: [],
       unused_only: true,
-      text_filter: ""
+      text_filter: "",
+      selected: ""
     }
   }
 
@@ -39,17 +40,45 @@ class QuestionList extends React.Component {
     this.setState({text_filter: value})
   }
 
+  set_selected = (question_id) => {
+    this.setState({selected: question_id});
+  }
+
+  set_value = (question_id, key, value) => {
+    let q = null
+    for (const qindex in this.state.questions) {
+      const question = this.state.questions[qindex]
+      console.log(question)
+      if (question.id === question_id) {
+        q = question
+        break
+      }
+    }
+
+    if (q === null) {
+      console.log("could not update data (question_id " + question_id + " not found)")
+    }
+    else {
+      q[key] = value
+      this.setState({questions: this.state.questions});
+    }
+
+
+  }
+
 
   render() {
     const questions = this.state.questions.map((question, index) => (
-            <Question key={question.id} category={question.category}
-                  answer={question.answer} question={question.question} />))
+            <Question key={question.id} id={question.id} category={question.category}
+                  answer={question.answer} question={question.question}
+                  selected={(this.state.selected === question.id) ? true : false}
+                  set_selected={this.set_selected} set={this.set_value} />))
 
     return (
-      <div> 
+      <div>
         <input name="text_filter" value={this.state.text_filter} onChange={this.set_text_filter} placeholder="Text filters"/>
         <input type="checkbox" name="text_filter" checked={this.state.unused_only} onChange={this.set_unused_only} />
-        <div className="question_list"> 
+        <div className="question_list">
           {questions}
   		  </div>
   	  </div>
