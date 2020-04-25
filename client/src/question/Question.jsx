ca8fd81df6a61c352e672c30e3ec2d4098760a42
JD Arthur <jdarthur@gatech.edu>
2020-04-25 13:55:19 -0400

    WIP: question selected

--- client/src/question/Question.jsx
+++ client/src/question/Question.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import './Question.css';
 
 class Question extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: false
+    }
+  }
+
+  set_selected = (event) => {
+    this.setState({selected: true})
+  }
+
+
   render() {
+    const containerClass = "question_container" + (this.state.selected ? " selected" : "")
+    console.log(containerClass)
     return (
-      <div className="question_container"> 
+      <div className={containerClass}
+           onClick={this.set_selected}> 
   		<div className="category">
             {this.props.category}
         </div>
