3fa4ce4424a5409a8af9c320ad1fc077f8e82c6a
JD Arthur <jdarthur@gatech.edu>
2020-05-15 19:10:36 -0400

    skeleton UI components

---
+++ client/src/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class Homepage extends React.Component {
+    render() {
+      return (
+        <div className="homepage">
+           <button>New Game</button>
+        </div>
+      );
+    }
+  }
+
+  export default Homepage;
