b9d618d2c0e82762cdbb5b1f76d08ee04ecddedb
John Arthur <jarthur@dispersive.io>
2022-02-09 19:45:35 -0500

    fixed react-markdown stuff

--- client/src/question/FormattedQuestion.jsx
+++ client/src/question/FormattedQuestion.jsx
@@ -8,17 +8,17 @@ class FormattedQuestion extends React.Component {
 
     render() {
         const renderers = {
-            image: ({ alt, src }) => (
+            img: ({ alt, src }) => (
                 <img alt={alt} src={src} title={alt}
                     style={{ maxWidth: this.props.max_width, maxHeight: '30vh' }} />),
-            paragraph: ({ value, children }) => (<p style={{ "marginBottom": 5 }} > {children} </p>),
+            p: ({ value, children }) => (<p style={{ "marginBottom": 5 }} > {children} </p>),
             blockquote: (({ value, children }) => (<blockquote style={{ paddingRight: 10 }} > {children} </blockquote>)),
             // text: ({ value, children }) => (<p style={{ "marginBottom": 5 }} > {children} </p>)
         };
         return (
             <div>
-                <ReactMarkdown source={this.props.question} renderers={renderers} />
-                <ReactMarkdown source={this.props.answer} renderers={renderers} className="answer" />
+                <ReactMarkdown components={renderers}  children={this.props.question}/>
+                <ReactMarkdown components={renderers} className="answer"  children={this.props.answer}/>
             </div>
         );
     }
