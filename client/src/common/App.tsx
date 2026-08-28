d0497ee03b7e3aac1a23bcfbe3d354ce0eb5f38b
John Arthur <jarthur@dispersive.io>
2026-08-28 15:31:45 -0400

    Reorder editor menu so Categories comes first, matching the game-creation flow

--- client/src/common/App.tsx
+++ client/src/common/App.tsx
@@ -123,6 +123,9 @@ export default function App() {
                 </Menu.Item>
 
                 <SubMenu key="sub1" icon={<FormOutlined/>} title="Editor" disabled={!showEditor}>
+                  <Menu.Item key="6" disabled={!showEditor}>
+                      <Link to={"categories"}>{CATEGORY}</Link>
+                  </Menu.Item>
                   <Menu.Item key="2" disabled={!showEditor}>
                     <Link to={"questions"}>{QUESTION}</Link>
                   </Menu.Item>
@@ -135,9 +138,7 @@ export default function App() {
                   <Menu.Item key="5" disabled={!showEditor}>
                       <Link to={"collections"}>{COLLECTION}</Link>
                   </Menu.Item>
-                  <Menu.Item key="6" disabled={!showEditor}>
-                      <Link to={"categories"}>{CATEGORY}</Link>
-                  </Menu.Item>
+
                 </SubMenu>
 
                   <Menu.Item key="7" style={{float: "right"}} className="nohover">
